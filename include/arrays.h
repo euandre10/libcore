43288f712fedd0cef1979132add30faf3d134207
euandre10 <andre_araujo@live.com>
2025-07-31 14:02:33 -0300

    newnew

--- include/arrays.h
+++ include/arrays.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 
+/* === TYPEDEFS AND STRUCTS === */
 typedef enum {
 		INT,
 		FLOAT,
@@ -8,12 +9,13 @@ typedef enum {
 		STRING
 } arrType;
 
-
 typedef struct {
 		void *arr;
 		arrType type;
 		size_t capacity, num_elem;
 } Array;
 
+/* === ARRAY FUNCTIONS === */
 Array	*arr_create(arrType datatype, size_t arrSize);
 void	arr_free(Array *arr);
+void    arr_push(Array *arr, void *value);
--- source/arrays_h/arr_free.c
+++ source/arrays_h/arr_free.c
@@ -1,5 +1,10 @@
 #include "arrays.h"
 
+/*
+ * @brief Free all memory allocated for the array, according to it's type.
+ * @param arr Struct that contains the array and its infos.
+ * */
+
 void	arr_free(Array *arr) {
 	if (arr->type == STRING) {
 		for (size_t i = 0; i < arr->capacity; i++)
